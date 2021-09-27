888fd9eb14a21b4f059cce2cff92b2d4106a8a3f
Kseniya Chechulova <44057113+yainesk99@users.noreply.github.com>
2021-09-28 00:20:22 +0300

    Add files via upload

---
+++ CoreTaskTemplate-master1/src/main/java/jm/task/core/jdbc/Main.java
@@ -0,0 +1,62 @@
+package jm.task.core.jdbc;
+
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+import jm.task.core.jdbc.util.Util;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class Main {
+
+    public static void main(String[] args) throws SQLException {
+     Util util = Util.getInstance();
+     util.getConnection();
+     UserService service = new UserServiceImpl();
+     service.createUsersTable();
+
+
+     User user1 = new User("Ivan", "Ivanov", (byte) 5);
+     User user2 = new User("Kseniya", "Chechulova", (byte) 22);
+     User user3 = new User("Oksana", "Markitan", (byte) 33);
+     User user4 = new User("Artsiom", "Makartsou", (byte) 23);
+
+     UserService service1 = new UserServiceImpl();
+     service1.saveUser(user1.getName(), user1.getLastName(), user1.getAge());
+
+     UserService service2 = new UserServiceImpl();
+     service2.saveUser(user2.getName(), user2.getLastName(), user2.getAge());
+
+     UserService service3 = new UserServiceImpl();
+     service3.saveUser(user3.getName(), user3.getLastName(), user3.getAge());
+
+     UserService service4 = new UserServiceImpl();
+     service4.saveUser(user4.getName(), user4.getLastName(), user4.getAge());
+
+     UserService service5 = new UserServiceImpl();
+     service5.removeUserById(1);
+
+     UserService service6 = new UserServiceImpl();
+     service6.dropUsersTable();
+
+     UserService service7 = new UserServiceImpl();
+     service7.cleanUsersTable();
+
+     UserService service8 = new UserServiceImpl();
+     service8.getAllUsers();
+
+       }
+    }
+
+
+
+
+
+
+
+
